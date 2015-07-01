7761a87fb32c2611ccc43e49cf6b81a03f9195b5
Jesper Anneback <jesper.anneback@gmail.com>
2015-07-01 14:59:39 +0200

    1st commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("It's alive!");
+    response.end();
+}).listen(3000);/**
+ * Created by anneback on 01/07/15.
+ */
